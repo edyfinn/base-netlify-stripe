2d4465f008dd5c243aa9f8f4092da2281cfdb461
David <product@edyflow.com>
2024-02-14 09:31:57 +0100

    Test claves.

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -6,8 +6,10 @@ exports.handler = async () => {
     };*/
 
     const mySecret = process.env.STRIPE_SECRET_KEY;
+    const mySecret1 = process.env.FAUNA_SERVER_KEY
+    const mySecret2 = process.env.STRIPE_DEFAULT_PRICE_PLAN
     return {
       statusCode: 200,
-      body: `La clave de stripe es: ${mySecret}`
+      body: `La clave de stripe es: ${mySecret} \n ${mySecret1} \n ${mySecret2}`
     };
   };
