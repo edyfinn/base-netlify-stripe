94a0306f3b69d33356cd598976918e9590850754
David <product@edyflow.com>
2024-04-01 20:40:25 +0200

    Acceso a FaunaDB correcto ;)

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -1,4 +1,7 @@
 const fetch = require('node-fetch');
+const faunaDB = require('faunadb');
+var q = faunaDB.query
+
 exports.handler = async () => {
     /*return {
       statusCode: 200,
@@ -8,8 +11,25 @@ exports.handler = async () => {
     const mySecret = process.env.STRIPE_SECRET_KEY;
     const mySecret1 = process.env.FAUNA_SERVER_KEY_V10
     const mySecret2 = process.env.STRIPE_DEFAULT_PRICE_PLAN
+
+    var client = new faunaDB.Client({
+      secret: process.env.FAUNA_BD_STRIPE,
+      domain: 'db.eu.fauna.com',
+       scheme: 'https',
+    })
+
+    var createP = client.query(
+      q.Create(q.Collection('UsuariosBuenos'), { data: { netlifyID: 'Cliente_Netlify' , stripeID: 'Cliente_Sprite' } })
+    )
+
+    
+    /*createP.then(function(response) {
+      console.log(response.ref) // Would log the ref to console.
+    })*/
+
+
     return {
       statusCode: 200,
-      body: `La clave de stripe es: ${mySecret} \n ${mySecret1} \n ${mySecret2}`
+      body: `La clave de stripe es: ${mySecret} \n ${mySecret1} \n ${mySecret2} \n ${createP}`
     };
   };
--- functions/utils/fauna.js
+++ functions/utils/fauna.js
@@ -1,7 +1,59 @@
 const fetch = require('node-fetch');
+import { Client, fql, FaunaError } from "faunadb";
 
-exports.faunaFetch = async ({ query, variables }) => {
-  return await fetch('https://db.eu.fauna.com/', { //https://graphql.eu.fauna.com/graphql
+
+//const client = new Client();
+// To configure your client:
+const client = new Client({
+   secret: process.env.FAUNA_BD_STRIPE,
+   domain: 'db.eu.fauna.com',
+    scheme: 'https',
+ });
+ var q = faunadb.query;
+
+ const collectionName = "UsuariosBuenos";
+ const collectionExists = (name) => fql`Collection.byName(${name}) != null`;
+ // Build query that uses the previous var and sub-query
+ exports.faunaFetch = async () => {
+  const respuesta = await  client.query(
+    q.Create(q.Collection('UsuariosBuenos'), { data: { netlifyID: 'testValue' , stripeID: 'Testeo' } })
+  );
+  alert(respuesta);
+
+    /*const upsertCollectionQuery = fql`
+    if (${collectionExists(collectionName)}) {
+      "Collection already exists"
+    } else {
+      Collection.create({ name: ${collectionName} })
+      "Collection created"
+    }
+    `;
+    // Run the query
+    const response = await client.query(upsertCollectionQuery);
+    console.log(response.data);
+
+    client.close();*/
+ }
+
+
+
+exports.faunaFetch2 = async ({ query, variables }) => {
+
+
+  
+
+  /*client.query(
+    q.Collection('UsuariosBuenos')
+  )
+  .then((res) => res.json())
+  .catch(function (err) { console.log('Error:', err) });*/
+
+  /*q.If(
+    q.Exists(q.Collection('Todos')),
+    null,
+    q.CreateCollection({ name: 'Todos' }));*/
+
+  /*return await fetch('https://db.eu.fauna.com/', { //https://graphql.eu.fauna.com/graphql
     method: 'POST',
     headers: {
       Authorization: `Bearer ${process.env.FAUNA_SERVER_KEY_V10}`,
@@ -12,6 +64,6 @@ exports.faunaFetch = async ({ query, variables }) => {
     }),
   })
     .then((res) => res.json())
-    .catch((err) => console.error(JSON.stringify(err, null, 2)));
+    .catch((err) => console.error(JSON.stringify(err, null, 2)));*/
 };
 
