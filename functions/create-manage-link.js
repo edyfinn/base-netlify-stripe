04a1411092a5d51ef3c2dd15b494b6b510b363fe
David <product@edyflow.com>
2024-04-18 10:59:12 +0200

    Prueba cliente directamente.

--- functions/create-manage-link.js
+++ functions/create-manage-link.js
@@ -32,7 +32,7 @@ exports.handler = async (_event, context) => {
   const { stripeID } = JSON.stringify(respuesta[0]);//result.data.getUserByNetlifyID;
   
   const link = await stripe.billingPortal.sessions.create({
-    customer: stripeID,
+    customer: 'cus_PuE4Xz1gYEVFRT',
     return_url: process.env.URL,
   });
 
