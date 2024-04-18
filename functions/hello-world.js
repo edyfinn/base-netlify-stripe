79226c2027e79b05252307c7d39e6c0699903dd7
David <product@edyflow.com>
2024-04-18 13:18:50 +0200

    Prueba solo una.

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -16,19 +16,19 @@ exports.handler = async () => {
     const mySecret1 = process.env.FAUNA_SERVER_KEY_V10
     const mySecret2 = process.env.STRIPE_DEFAULT_PRICE_PLAN
 
-    /*var client = new faunaDB.Client({
+    var client = new faunaDB.Client({
       secret: process.env.FAUNA_BD_STRIPE,
       domain: 'db.eu.fauna.com',
       scheme: 'https',
-    });*/
+    });
 
-    /*let respuesta = await client.query(
+    let respuesta = await client.query(
       q.Select('data', q.Paginate(q.Match(q.Index('getUsuarioNetlifyID'), 'ba31a0e6-dac9-425a-9b46-246dfd4e906f')))
-    );*/
+    );
     //usuario = JSON.stringify(usuarioStripe);
     //console.log(JSON.stringify(respuesta[0]));
     var prueba = 'testeo';
-    const [stripeID, link] = await Promise.allSettled([getClienteStripe(), getLinkPago(prueba)]);
+    const [link, stripeID] = await Promise.allSettled([getLinkPago(prueba), getClienteStripe()]);
     
     //console.log("respuesta: ", respuesta);
 
@@ -45,7 +45,7 @@ exports.handler = async () => {
       statusCode: 200,
       //body: `La clave de stripe es: ${mySecret} \n ${mySecret1} \n ${mySecret2} \n ${createP}`,
       //body: `Respuesta Query faunaDB: ${JSON.stringify(respuesta[0])}`,
-      body: `Respuesta Query en PARALELO: ${stripeID} \n  ${JSON.stringify(link.url)}`,
+      body: `Respuesta Query en PARALELO: ${JSON.stringify(link)}`,
     };
 };
 
@@ -65,15 +65,10 @@ async function getLinkPago(cliente) {
 }
 
 async function getClienteStripe() {
-  var client = new faunaDB.Client({
-    secret: process.env.FAUNA_BD_STRIPE,
-    domain: 'db.eu.fauna.com',
-    scheme: 'https',
-  });
-
   const respuesta = await client.query(
     q.Select('data', q.Paginate(q.Match(q.Index('getUsuarioNetlifyID'), 'ba31a0e6-dac9-425a-9b46-246dfd4e906f')))
   );
+  console.log(JSON.stringify(respuesta[0]))
   return JSON.stringify(respuesta[0]);
 }
 
