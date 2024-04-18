0f9f8b95d362e31dcc5427403c651b112d491919
David <product@edyflow.com>
2024-04-18 09:41:26 +0200

    Prueba simple recuperación datos faunadb.

--- functions/hello-world.js
+++ functions/hello-world.js
@@ -25,14 +25,12 @@ exports.handler = async () => {
       q.Map(
           q.Paginate(
               q.Match(
-                  q.Index('getUsuarioNetlifyID'),
-                  ['netlifyID','43be6508-e3b1-4349-9d54-379e403ee7b3']
+                  q.Index('getUsuarioNetlifyID'),'43be6508-e3b1-4349-9d54-379e403ee7b3'
               )
-          ),
-          q.Lambda('stripeID', q.Get(q.Var('stripeID')))
+          )
       )
     );
-    console.log("respuesta: ", respuesta);
+    //console.log("respuesta: ", respuesta);
 
     /*var createP = client.query(
       q.Create(q.Collection('UsuariosBuenos'), { data: { netlifyID: 'Cliente_Netlify_hola' , stripeID: 'Cliente_Sprite_hola' } })
