ff665ec143272a273b8552f0b625517ca368285b
edyfinn <product@edyflow.com>
2025-06-13 20:40:54 +0200

    redirect to edyflow

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = (config) => {
   config.addPassthroughCopy('src/css');
   config.addPassthroughCopy('src/images');
   config.addPassthroughCopy('src/js');
+  eleventyConfig.addPassthroughCopy("_redirects");
 
   return {
     dir: {
